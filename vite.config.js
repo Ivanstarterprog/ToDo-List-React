f3b4326f4031ebedcd9d484b0d5f0fdc429dc778
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2025-11-08 21:42:19 +0700

    Добавление Allias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@styles": path.resolve(__dirname, "src/shared/styles"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+});

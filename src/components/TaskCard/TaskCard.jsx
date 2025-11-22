4e7214287de1bef9ae5feae9000ce4201afdbf86
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2025-11-22 12:13:35 +0700

    Добавление функциональности открытия кнопок под карточкой задачи

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,17 +1,42 @@
 import styles from "./TaskCard.module.css";
-import TaskCardInformation from "../TaskCardInformation";
+import TaskCardInformation from "@components/TaskCardInformation";
 import DeleteTaskButton from "@components/DeleteTaskButton";
-import TaskCardButtonsHolder from "../TaskCardButtonsHolder";
-export const TaskCard = (task) => {
+import TaskCardButtonsHolder from "@components/TaskCardButtonsHolder";
+import { useState, useRef, useEffect } from "react";
+export const TaskCard = ({ task }) => {
+  const [isButtonsVisible, setIsButtonsVisible] = useState(false);
+  const cardRef = useRef(null);
+
+  const handleCardClick = () => {
+    setIsButtonsVisible(!isButtonsVisible);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (cardRef.current && !cardRef.current.contains(event.target)) {
+        setIsButtonsVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div>
-      <div className={styles.task_card_container}>
+    <div ref={cardRef}>
+      <div className={styles.task_card_container} onClick={handleCardClick}>
         <div className={styles.task__card}>
           <TaskCardInformation task={task}></TaskCardInformation>
-          <DeleteTaskButton taskID={task.id}></DeleteTaskButton>
+          <DeleteTaskButton
+            onClick={(e) => e.stopPropagation()}
+          ></DeleteTaskButton>
         </div>
       </div>
-      <TaskCardButtonsHolder></TaskCardButtonsHolder>
+      <TaskCardButtonsHolder
+        isVisible={isButtonsVisible}
+      ></TaskCardButtonsHolder>
     </div>
   );
 };
--- src/components/TaskCardButtonsHolder/TaskCardButtonsHolder.jsx
+++ src/components/TaskCardButtonsHolder/TaskCardButtonsHolder.jsx
@@ -1,24 +1,30 @@
-import { TaskCardButton } from "../TaskCardButton/TaskCardButton";
-import styles from "./TaskCardButton.module.css";
+import TaskCardButton from "@components/TaskCardButton";
+import styles from "./TaskCardButtonsHolder.module.css";
 import shareImage from "@assets/img/share.svg";
 import informationImage from "@assets/img/information.svg";
 import editImage from "@assets/img/edit.svg";
 
-export const TaskCardButtonsHolder = () => {
+export const TaskCardButtonsHolder = ({ isVisible }) => {
   return (
-    <div className={styles.task__card__buttons_container}>
-      <TaskCardButton
-        image={shareImage}
-        image_alt="Поделиться"
-      ></TaskCardButton>
-      <TaskCardButton
-        image={informationImage}
-        image_alt="Информация о задаче"
-      ></TaskCardButton>
-      <TaskCardButton
-        image={editImage}
-        image_alt="Изменить задачу"
-      ></TaskCardButton>
+    <div
+      className={`${styles.task__card__buttons_container} ${
+        isVisible ? styles.visible : ""
+      }`}
+    >
+      <div>
+        <TaskCardButton
+          image={shareImage}
+          image_alt="Поделиться"
+        ></TaskCardButton>
+        <TaskCardButton
+          image={informationImage}
+          image_alt="Информация о задаче"
+        ></TaskCardButton>
+        <TaskCardButton
+          image={editImage}
+          image_alt="Изменить задачу"
+        ></TaskCardButton>
+      </div>
     </div>
   );
 };

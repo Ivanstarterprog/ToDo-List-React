17e9d37d090f2153efae7e7efaae7dcac1519a7a
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2026-01-05 01:17:42 +0700

    Добавление ссылки на папку с хуками

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       "@styles": path.resolve(__dirname, "src/shared/styles"),
       "@components": path.resolve(__dirname, "src/components"),
       "@assets": path.resolve(__dirname, "src/assets"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
     },
   },
 });
